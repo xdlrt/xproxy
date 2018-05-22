2f492dca8dd9cc98c39a0fdca26db2e4f680e275
yeshu.lrt <yeshu.lrt@alibaba-inc.com>
2018-05-22 14:40:39 +0800

    :bug: fix bug and add combined support

--- src/forward.js
+++ src/forward.js
@@ -1,7 +1,7 @@
 import { CHECK_IS_LEGAL, ORIGIN_URL_REG } from './scripts/constants/regExp';
 
-let lastRequestId = null;
-const urls = new Array(200); // for cache
+window.lastRequestId = null;
+window.urls = new Array(200); // for cache
 
 const redirectToMatchingRule = (details) => {
   const rules = window.xproxyConfig.proxyList;
@@ -14,40 +14,44 @@ const redirectToMatchingRule = (details) => {
   }
 
   // check if url is legal
-  if (CHECK_IS_LEGAL.test(originUrl) && urls.indexOf(originUrl) < 0) {
-    urls.shift();
-    urls.push(originUrl);
+  if (CHECK_IS_LEGAL.test(originUrl) && window.urls.indexOf(originUrl) < 0) {
+    window.urls.shift();
+    window.urls.push(originUrl);
   }
 
-  try {
-    rules.forEach(rule => {
-      let reg = rule.url;
-      let isMatched = false;
-      // check if [ ] ( ) \ * ^ $ exist
-      if (ORIGIN_URL_REG.test(rule.url)) {
-        // transform originUrl into regExp
-        // check if ?? exist, if true, transform ?? to regExp format
-        reg = new RegExp(originUrl.replace('??', '\\?\\?'), 'i');
-        isMatched = reg.test(originUrl);
-      } else {
-        // originUrl rules directly include rule.url
-        isMatched = originUrl.indexOf(reg) > -1;
-      }
+  rules.forEach(rule => {
 
-      if (isMatched && details.requestId !== lastRequestId) {
-        redirectUrl = originUrl.replace(reg, rule.redirectUrl);
-      }
+    if (!rule.url || !rule.redirectUrl) {
+      return;
+    }
 
-      if (!redirectUrl) {
-        redirectUrl = originUrl;
-      }
+    let reg = rule.url;
+    let isMatched = false;
 
-    });
-  } catch (e) {
-    console.error('rule match error', e);
+    // check if [ ] ( ) \ * ^ $ exist
+    if (ORIGIN_URL_REG.test(rule.url)) {
+      // transform originUrl into regExp
+      // check if ?? exist, if true, transform ?? to regExp format
+      reg = new RegExp(reg.replace('??', '\\?\\?'), 'i');
+      isMatched = reg.test(originUrl);
+    } else {
+      // originUrl rules directly include rule.url
+      isMatched = originUrl.indexOf(reg) > -1;
+    }
+
+    if (isMatched && details.requestId !== window.lastRequestId) {
+      redirectUrl = redirectUrl
+        ? redirectUrl.replace(reg, rule.redirectUrl)
+        : originUrl.replace(reg, rule.redirectUrl);
+    }
+
+  });
+
+  window.lastRequestId = details.requestId;
+
+  if (!redirectUrl) {
+    redirectUrl = originUrl;
   }
-
-  lastRequestId = details.requestId;
 
   return redirectUrl === details.url ? {} : { redirectUrl };
 };
